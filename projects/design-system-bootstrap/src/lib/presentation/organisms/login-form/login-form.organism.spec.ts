ee4d98e6689032e880dfe7c22904f98622bc6e39
Juan Vargas <juan.vargas03@uceva.edu.co>
2026-09-11 18:32:53 -0500

    test(organisms): pruebas unitarias de LoginFormOrganism

--- projects/design-system-bootstrap/src/lib/presentation/organisms/login-form/login-form.organism.spec.ts
+++ projects/design-system-bootstrap/src/lib/presentation/organisms/login-form/login-form.organism.spec.ts
@@ -1,5 +1,5 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed } from '@angular/core';
+import { By } from '@angular/platform-browser';
 import { LoginFormOrganism } from './login-form.organism';
 
 describe('LoginFormOrganism', () => {
@@ -8,16 +8,49 @@ describe('LoginFormOrganism', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LoginFormOrganism]
-    })
-    .compileComponents();
+      imports: [LoginFormOrganism],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(LoginFormOrganism);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('Debería crear el componente', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Debería actualizar los valores de email y password', () => {
+    component.onEmailChange('test@ejemplo.com');
+    component.onPasswordChange('123456');
+
+    expect((component as any).email).toBe('test@ejemplo.com');
+    expect((component as any).password).toBe('123456');
+  });
+
+  it('Debería emitir formSubmit cuando email y password tienen valor', () => {
+    const spy = jest.spyOn(component.formSubmit, 'emit');
+    component.onEmailChange('test@ejemplo.com');
+    component.onPasswordChange('123456');
+
+    component.onSubmit();
+
+    expect(spy).toHaveBeenCalledWith({
+      email: 'test@ejemplo.com',
+      password: '123456',
+    });
+  });
+
+  it('No debería emitir formSubmit si falta algún campo', () => {
+    const spy = jest.spyOn(component.formSubmit, 'emit');
+
+    component.onEmailChange('test@ejemplo.com');
+    component.onSubmit();
+    expect(spy).not.toHaveBeenCalled();
+
+    component.onEmailChange('');
+    component.onPasswordChange('123456');
+    component.onSubmit();
+    expect(spy).not.toHaveBeenCalled();
   });
 });
